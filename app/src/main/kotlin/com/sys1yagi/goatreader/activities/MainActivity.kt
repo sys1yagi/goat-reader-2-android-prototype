1a72d145e4eb292121444d982d2bb7530890bb2d
sys1yagi <sylc.yagi@gmail.com>
2014-07-05 14:01:08 +0900

    Add NavigationDrawer.

--- app/src/main/kotlin/com/sys1yagi/goatreader/activities/MainActivity.kt
+++ app/src/main/kotlin/com/sys1yagi/goatreader/activities/MainActivity.kt
@@ -2,21 +2,28 @@ package com.sys1yagi.goatreader.activities
 
 import android.os.Bundle
 import com.sys1yagi.goatreader.tools.ContextBus
-import com.sys1yagi.goatreader.R
 import android.support.v7.app.ActionBarActivity
 import com.sys1yagi.goatreader.fragments.TopListFragment
 import com.sys1yagi.goatreader.tools.Cron
+import com.sys1yagi.goatreader.extensions.*
+import android.support.v4.app.ActionBarDrawerToggle
+import android.support.v4.widget.DrawerLayout
+import com.sys1yagi.goatreader.R
+import android.view.View
+import android.widget.ListView
+import android.view.MenuItem
 
 public class MainActivity() : ActionBarActivity() {
 
+    var drawerToggle: ActionBarDrawerToggle? = null
+    val drawerLayout: DrawerLayout by viewInjector(R.id.drawer_layout)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupActionBar()
-
         Cron.start(this)
-        ContextBus.getInstance(this)?.register(this);
+        ContextBus.getInstance(this)?.register(this) ;
 
         setContentView(R.layout.activity_main)
+        setupActionBar()
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     ?.beginTransaction()
@@ -33,7 +40,34 @@ public class MainActivity() : ActionBarActivity() {
     fun setupActionBar() {
         val actionBar = getSupportActionBar()
         actionBar?.setLogo(getResources()?.getDrawable(R.drawable.ic_theme_with_shadow))
+        drawerToggle = object : ActionBarDrawerToggle(
+                this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+            override fun onDrawerClosed(view: View?) {
+                super.onDrawerClosed(view)
+            }
+
+            override fun onDrawerOpened(drawerView: View?) {
+                super.onDrawerOpened(drawerView)
+            }
+        }
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        actionBar?.setDisplayHomeAsUpEnabled(true);
+        actionBar?.setHomeButtonEnabled(true);
     }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle?.syncState();
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle?.onOptionsItemSelected(item)!!) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 
 
     //    Subscribe fun onMoge(a: String) {
--- app/src/main/kotlin/com/sys1yagi/goatreader/extensions/ActivityExtension.kt
+++ app/src/main/kotlin/com/sys1yagi/goatreader/extensions/ActivityExtension.kt
@@ -2,35 +2,24 @@ package com.sys1yagi.goatreader.extensions
 
 import android.view.View
 import android.app.Activity
+import com.sys1yagi.goatreader.tools.Logger
 
 public trait Injector<T> {
     fun get(thisRef: Any, prop: PropertyMetadata): T
 }
 
-public fun <T : View> Activity.viewInjector(resId: Int? = null): Injector<T> = object: Injector<T> {
+public fun <T : View> Activity.viewInjector(resId: Int): Injector<T> = object : Injector<T> {
 
     private var view: T? = null
 
     override fun get(thisRef: Any, prop: PropertyMetadata): T {
         if (view == null) {
-            view = getView(resId, prop.name)
+            view = getView(resId)
         }
 
         return view!!
     }
 
-    private fun <T : View> Activity.getView(resId: Int?, name: String): T =
-            (if (resId != null)
-                findViewById(resId)
-            else
-                findViewByName(name)) as T
+    private fun <T : View> Activity.getView(resId: Int): T = findViewById(resId) as T
 
-    private fun Activity.findViewByName(name: String): View? =
-            try {
-                val resIds = Class.forName(getPackageName() + ".R\$id")
-                val resId = resIds?.getField(name)?.get(resIds) as Int
-                findViewById(resId)
-            } catch(e: Exception) {
-                null
-            }
 }
